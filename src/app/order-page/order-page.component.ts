7240f19e9c23218fdb7047943d5c7e1c10f6d627
Tolnai Tamás <h165209@stud.u-szeged.hu>
2025-05-01 15:12:14 +0200

    Creating the page components

---
+++ src/app/order-page/order-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-page',
+  imports: [],
+  templateUrl: './order-page.component.html',
+  styleUrl: './order-page.component.scss'
+})
+export class OrderPageComponent {
+
+}
