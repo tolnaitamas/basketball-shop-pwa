298f83c622e96ce5905f68a265834d67b9902263
Tolnai Tamás <h165209@stud.u-szeged.hu>
2025-05-01 19:35:37 +0200

    Creating the navbar in app component and making the routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ViewChild } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    MatSidenavModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    RouterLink,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'basketball-shop-pwa';
+
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
+  constructor(private router: Router) {}
+
+  sidenavOpened = false;
+
+  navigateTo(route: string): void {
+    this.router.navigate([route]).then(() => {
+      this.sidenav.close();
+    });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,50 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'main',
+    loadComponent: () =>
+      import('./main-page/main-page.component').then(
+        (c) => c.MainPageComponent
+      ),
+  },
+  {
+    path: 'product',
+    loadComponent: () =>
+      import('./product-page/product-page.component').then(
+        (c) => c.ProductPageComponent
+      ),
+  },
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('./products-page/products-page.component').then(
+        (c) => c.ProductsPageComponent
+      ),
+  },
+  {
+    path: 'profile',
+    loadComponent: () =>
+      import('./profile-page/profile-page.component').then(
+        (c) => c.ProfilePageComponent
+      ),
+  },
+  {
+    path: 'cart',
+    loadComponent: () =>
+      import('./cart-page/cart-page.component').then(
+        (c) => c.CartPageComponent
+      ),
+  },
+  {
+    path: 'order',
+    loadComponent: () =>
+      import('./order-page/order-page.component').then(
+        (c) => c.OrderPageComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'main',
+  },
+];
