2f761635beae608b653f345a4fb29bbc95a8de2e
Tolnai Tamás <h165209@stud.u-szeged.hu>
2025-05-02 10:42:45 +0200

    Creating the footer and placing in return and main page

--- src/app/return-page/return-page.component.ts
+++ src/app/return-page/return-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-return-page',
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './return-page.component.html',
-  styleUrl: './return-page.component.scss'
+  styleUrl: './return-page.component.scss',
 })
-export class ReturnPageComponent {
-
-}
+export class ReturnPageComponent {}
