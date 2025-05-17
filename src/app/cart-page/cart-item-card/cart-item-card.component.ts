eab17cde44233cf66070c472638a4c1a5f6c7d3a
Tolnai Tamás <h165209@stud.u-szeged.hu>
2025-05-17 21:48:15 +0200

    Some change in design

--- src/app/cart-page/cart-item-card/cart-item-card.component.ts
+++ src/app/cart-page/cart-item-card/cart-item-card.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../../shared/types/product';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-cart-item-card',
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule],
   templateUrl: './cart-item-card.component.html',
   styleUrl: './cart-item-card.component.scss',
 })
