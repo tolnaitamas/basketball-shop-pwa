63214a3e85d61cdf0eb099073302bd587d0a5de0
Tolnai Tamás <h165209@stud.u-szeged.hu>
2025-05-17 17:36:05 +0200

    Implementing the cart item card and the rest of the page

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -1,11 +1,60 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../shared/footer/footer.component';
+import { CartItemCardComponent } from './cart-item-card/cart-item-card.component';
+import { Product } from '../shared/types/product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart-page',
-  imports: [],
+  imports: [FooterComponent, CartItemCardComponent, CommonModule],
   templateUrl: './cart-page.component.html',
-  styleUrl: './cart-page.component.scss'
+  styleUrl: './cart-page.component.scss',
 })
 export class CartPageComponent {
-
+  products: Product[] = [
+    {
+      id: 1,
+      sex: 'Férfi',
+      brand: 'Jordan',
+      name: 'Air Jordan 1 Low White/Metallic Gold-Black',
+      imageUrl: 'main-page/asd.webp',
+      category: 'clothes',
+      price: 53999,
+      sizes: [40, 41, 45, 45.5],
+      selectedSize: 40,
+    },
+    {
+      id: 2,
+      sex: 'Férfi',
+      brand: 'Nike',
+      name: 'Nike Air Max 97 Silver Bullet',
+      imageUrl: 'main-page/asd.webp',
+      category: 'shoes',
+      price: 74999,
+      sizes: [42, 43, 44],
+      selectedSize: 42,
+    },
+    {
+      id: 3,
+      sex: 'Női',
+      brand: 'Adidas',
+      name: 'Adidas UltraBoost 21',
+      imageUrl: 'main-page/asd.webp',
+      category: 'caps',
+      price: 86999,
+      sizes: [40, 41, 42, 43],
+      selectedSize: 41,
+    },
+    {
+      id: 4,
+      sex: 'Férfi',
+      brand: 'Puma',
+      name: 'Puma RS-X3',
+      imageUrl: 'main-page/asd.webp',
+      category: 'other',
+      price: 59999,
+      sizes: [43, 44, 45],
+      selectedSize: 44,
+    },
+  ];
 }
