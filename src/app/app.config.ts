8ac1287b9c92b230188d7d2fadf48e688df4c948
Tolnai Tamás <h165209@stud.u-szeged.hu>
2025-05-19 20:18:49 +0200

    install firebase package

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,6 +18,6 @@ export const appConfig: ApplicationConfig = {
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
-    }),
+    }), provideFirebaseApp(() => initializeApp({ projectId: "basketball-shop-pwa", appId: "1:65663420889:web:b4120b354d8ea0a21bb1c8", storageBucket: "basketball-shop-pwa.firebasestorage.app", apiKey: "AIzaSyCnNd3sqK5yIkZtPa_RLEK3SgL94OQ28RA", authDomain: "basketball-shop-pwa.firebaseapp.com", messagingSenderId: "65663420889" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
   ],
 };
