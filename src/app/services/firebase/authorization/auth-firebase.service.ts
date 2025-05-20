9dc5866e12e6eb6ea18467320b7aa0403cf3f01f
Tolnai Tamás <h165209@stud.u-szeged.hu>
2025-05-20 13:46:43 +0200

    Creating the guard and auth firebase

--- src/app/services/firebase/authorization/auth-firebase.service.ts
+++ src/app/services/firebase/authorization/auth-firebase.service.ts
@@ -1,9 +1,63 @@
 import { Injectable } from '@angular/core';
+import {
+  Auth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  User,
+  UserCredential,
+  UserProfile,
+} from '@angular/fire/auth';
+import { doc, docData, Firestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { Observable, switchMap, of, map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthFirebaseService {
+  currentUser$: Observable<User | null>;
 
-  constructor() { }
+  constructor(
+    private auth: Auth,
+    private router: Router,
+    private firestore: Firestore
+  ) {
+    this.currentUser$ = new Observable((observer) => {
+      onAuthStateChanged(this.auth, (user) => {
+        observer.next(user);
+      });
+    });
+  }
+
+  getUserProfile(): Observable<UserProfile | null> {
+    return this.currentUser$.pipe(
+      switchMap((user) => {
+        if (user) {
+          const userDocRef = doc(this.firestore, `users/${user.uid}`);
+          return docData(userDocRef) as Observable<UserProfile>;
+        } else {
+          return of(null);
+        }
+      })
+    );
+  }
+
+  login(email: string, password: string): Promise<UserCredential> {
+    return signInWithEmailAndPassword(this.auth, email, password).then(
+      (userCredential) => {
+        this.router.navigate(['/profile']);
+        return userCredential;
+      }
+    );
+  }
+
+  logout(): Promise<void> {
+    return this.auth.signOut().then(() => {
+      this.router.navigate(['/login']);
+    });
+  }
+
+  isLoggedIn(): Observable<boolean> {
+    return this.currentUser$.pipe(map((user) => !!user));
+  }
 }
--- src/app/shared/login-guard/login.guard.ts
+++ src/app/shared/login-guard/login.guard.ts
@@ -1,5 +1,19 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { tap, map } from 'rxjs';
+import { AuthFirebaseService } from '../../services/firebase/authorization/auth-firebase.service';
 
 export const loginGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authService = inject(AuthFirebaseService);
+  const router = inject(Router);
+
+  return authService.isLoggedIn().pipe(
+    tap((loggedIn) => {
+      if (!loggedIn) {
+        alert('Nincs jogosultság! Kérjük, jelentkezz be.');
+        router.navigate(['/login']);
+      }
+    }),
+    map((loggedIn) => loggedIn)
+  );
 };
