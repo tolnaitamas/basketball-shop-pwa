242334fbd675057ccfdc579b68241210a64b357e
Tolnai Tamás <h165209@stud.u-szeged.hu>
2025-05-21 10:39:12 +0200

    Fixing the total price calculation

--- src/app/components/cart-page/cart-item-card/cart-item-card.component.ts
+++ src/app/components/cart-page/cart-item-card/cart-item-card.component.ts
@@ -58,6 +58,7 @@ export class CartItemCardComponent {
 
       if (matchingProduct) {
         this.prize = matchingProduct.price;
+        this.product.price = this.prize * this.product.quantity;
       } else {
         console.warn(
           `Termék nem található a cache-ben ID alapján: ${this.product.id}`
